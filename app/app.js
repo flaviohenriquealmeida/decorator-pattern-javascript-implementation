d7d8d4c56fd793eb6a1a1de38571362d8c15e6da
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2018-02-16 10:07:24 -0200

    update

--- app/app.js
+++ app/app.js
@@ -2,15 +2,12 @@ import { Person } from './models/person.js';
 import { getFullNameWithHyphen } from './models/decorators.js';
 import { decorate } from './utils/decorate.js';
 
-const person1 = new Person('Flávio', 'Almeida');
-const person2 = new Person('Almeida', 'Flávio');
-
-person1.speak('Cangaceiro Javascript');
-console.log(person1.getFullName());
-
-decorate(person2, {
+decorate(Person, {
     getFullName: getFullNameWithHyphen
 });
 
+const person1 = new Person('Flávio', 'Almeida');
+const person2 = new Person('Almeida', 'Flávio');
+
+console.log(person1.getFullName());
 console.log(person2.getFullName());
-// Almeida-Flávio
--- app/utils/decorate.js
+++ app/utils/decorate.js
@@ -1,8 +1,31 @@
-export const decorate = (target, handler) => 
-    Object.keys(handler).forEach(propertyName => {
-        
-        const original = target[propertyName].bind(target);
+const isClass = source => 
+    typeof source === 'function' 
+        && /^\s*class\s+/.test(source.toString());
 
-        target[propertyName] = (...args) => 
-            handler[propertyName](original, propertyName, args);
-    });
+
+const decorateInstance = (target, handler, propertyName) => {
+
+    const original = target[propertyName].bind(target);
+    target[propertyName] = (...args) => 
+        handler[propertyName](original, propertyName, args);    
+    
+};
+
+const decorateClass = (target, handler, propertyName) => {
+
+    const original = target.prototype[propertyName];
+    target.prototype[propertyName] = function (...args) {
+        return handler[propertyName](original.bind(this), propertyName, args);
+    };  
+};
+
+export const decorate = (target, handler) => {
+
+    const isClazz = isClass(target);
+
+    Object.keys(handler).forEach(
+        propertyName => isClazz 
+            ? decorateClass(target, handler, propertyName)
+            : decorateInstance(target, handler, propertyName)
+    );
+};
