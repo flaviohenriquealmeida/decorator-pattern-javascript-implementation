0168e485d0898d7e099a736f6f56a000510fee0a
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2018-02-16 15:11:49 -0200

    update

--- app/utils/decorate.js
+++ app/utils/decorate.js
@@ -2,23 +2,19 @@ const isClass = source =>
     typeof source === 'function' 
         && /^\s*class\s+/.test(source.toString());
 
-const decorateInstance = (target, decorators, property) => {
+const decorateInstance = (target, decorator, property) => {
 
-    decorators.forEach(decorator => {
-        const method = target[property].bind(target);
-        target[property] = (...args) => 
-        decorator(method, property, args);    
-    });
+    const method = target[property].bind(target);
+    target[property] = (...args) => 
+        decorator(method, property, args);
 };
 
-const decorateClass = (target, decorators, property) => {
+const decorateClass = (target, decorator, property) => {
 
-    decorators.forEach(decorator => {
-        const method = target.prototype[property];
-        target.prototype[property] = function (...args) {
-            return decorator(method.bind(this), property, args);
-        };  
-    });
+    const method = target.prototype[property];
+    target.prototype[property] = function (...args) {
+        return decorator(method.bind(this), property, args);
+    };   
 };
 
 export const decorate = (target, handler) => {
@@ -26,9 +22,12 @@ export const decorate = (target, handler) => {
     const isClazz = isClass(target);
 
     Object.keys(handler).forEach(property => {
+
         const decorators = handler[property].reverse();
-        isClazz 
-            ? decorateClass(target, decorators, property)
-            : decorateInstance(target, decorators, property)
+        
+        decorators.forEach(decorator => isClazz 
+            ? decorateClass(target, decorator, property)
+            : decorateInstance(target, decorator, property)
+        ); 
     });    
 };
